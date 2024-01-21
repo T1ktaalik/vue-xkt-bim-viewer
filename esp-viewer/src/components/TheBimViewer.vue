9b7b2edbb75ed97e3ca51d4a3df42eae36dc7df8
Sergey_Oleynik <tiktaalik@yandex.ru>
2024-01-21 20:47:20 +0300

    21012023-3

--- esp-viewer/src/components/TheBimViewer.vue
+++ esp-viewer/src/components/TheBimViewer.vue
@@ -6,26 +6,33 @@
       <q-btn icon="info" flat @click="isInspectorOpen = !isInspectorOpen"></q-btn>
     </q-toolbar>
   </q-header>
-  <q-drawer id="Explorer" side='left' v-model='isExplorerOpen'> 
+  <q-drawer id="Explorer" side='left' v-model='isExplorerOpen' class="bg-red-2"> 
     <div>Обозреватель модели</div>
-    <q-option-group
+    <q-tabs
     v-model="explorerPanelName"
-    :options="explorerOptions"
-    inline
-    @click="showExplorerPanelName()">
-    </q-option-group>
-<!--  <q-tab-panels>
-  <q-tab-panel ></q-tab-panel>
-  <q-tab-panel ></q-tab-panel>
-  <q-tab-panel ></q-tab-panel>
-  <q-tab-panel ></q-tab-panel>
- </q-tab-panels> -->
+    active-color="indigo-3"
+   >
+   <q-tab v-for="item in explorerOptions"  :name="item" :label="item"></q-tab>
+  </q-tabs>
+ <q-tab-panels v-model="explorerPanelName">
+  <q-tab-panel v-for="item in explorerOptions" :name="item" :label="item">
+    <q-btn-toggle
+    :options="[ {label: 'показать'}, {label: 'скрыть'}]"
+    ></q-btn-toggle>
+</q-tab-panel>
+ </q-tab-panels>
   </q-drawer>
         <q-drawer id="Inspector" side='right' v-model='isInspectorOpen' > <div> Inspector </div> </q-drawer>
     <q-page-container>
-
-      <q-page class="bg-pink-4">
-        
+      <q-page class="bg-pink-4"> 
+        <q-btn-group>
+          <q-btn>Reset view</q-btn>
+          <q-btn>Toggle 3D / 2D</q-btn>
+          <q-btn>Toggle perspective /  ortho</q-btn>
+          <q-btn>View fit</q-btn>
+          <q-btn>Toggle first person mode</q-btn>
+          <q-btn>Show IFC spaces</q-btn>
+        </q-btn-group>
       </q-page>
     </q-page-container>
   </q-layout>
@@ -34,14 +41,6 @@
 import  { ref } from 'vue'
 const isExplorerOpen = ref(true)
 const isInspectorOpen = ref(true)
-const explorerOptions = ref([
-  {label: 'Models', value: 'Models'},
-  {label: 'Objects', value: 'Objects'},
-  {label: 'Classes', value: 'Classes'},
-  {label: 'Storeys', value: 'Storeys'}
-  ])
-  const explorerPanelName = ref('Models')
-  function showExplorerPanelName() {
-    console.log(explorerPanelName.value)
-  }
+const explorerOptions = ref([ 'Модели', 'Объекты', 'Классы', 'Уровни'])
+const explorerPanelName = ref('Модели')
 </script>
