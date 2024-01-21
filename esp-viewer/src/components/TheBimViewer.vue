364ea2f284e6f9bec18ca7f1e5c66e294138c790
Sergey <88627980+T1ktaalik@users.noreply.github.com>
2024-01-21 19:07:54 +0300

    21012024-2

--- esp-viewer/src/components/TheBimViewer.vue
+++ esp-viewer/src/components/TheBimViewer.vue
@@ -1,11 +1,47 @@
 <template>
   <q-layout>
-    <TheBimViewerHeader></TheBimViewerHeader>
+    <q-header>
+    <q-toolbar class="row justify-between">
+      <q-btn icon="account_tree" flat @click="isExplorerOpen = !isExplorerOpen"></q-btn>
+      <q-btn icon="info" flat @click="isInspectorOpen = !isInspectorOpen"></q-btn>
+    </q-toolbar>
+  </q-header>
+  <q-drawer id="Explorer" side='left' v-model='isExplorerOpen'> 
+    <div>Обозреватель модели</div>
+    <q-option-group
+    v-model="explorerPanelName"
+    :options="explorerOptions"
+    inline
+    @click="showExplorerPanelName()">
+    </q-option-group>
+<!--  <q-tab-panels>
+  <q-tab-panel ></q-tab-panel>
+  <q-tab-panel ></q-tab-panel>
+  <q-tab-panel ></q-tab-panel>
+  <q-tab-panel ></q-tab-panel>
+ </q-tab-panels> -->
+  </q-drawer>
+        <q-drawer id="Inspector" side='right' v-model='isInspectorOpen' > <div> Inspector </div> </q-drawer>
     <q-page-container>
-      <q-page class="bg-pink-4"></q-page>
+
+      <q-page class="bg-pink-4">
+        
+      </q-page>
     </q-page-container>
   </q-layout>
 </template>
 <script setup>
-import TheBimViewerHeader from './TheBimViewerHeader.vue'
+import  { ref } from 'vue'
+const isExplorerOpen = ref(true)
+const isInspectorOpen = ref(true)
+const explorerOptions = ref([
+  {label: 'Models', value: 'Models'},
+  {label: 'Objects', value: 'Objects'},
+  {label: 'Classes', value: 'Classes'},
+  {label: 'Storeys', value: 'Storeys'}
+  ])
+  const explorerPanelName = ref('Models')
+  function showExplorerPanelName() {
+    console.log(explorerPanelName.value)
+  }
 </script>
